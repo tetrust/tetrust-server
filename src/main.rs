b131fcd5b2c3da0a87996bada5358adb192f6848
myyrakle <sssang97@naver.com>
2022-11-13 15:21:57 +0900

    feat: 기본 서버 동작 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let port = 3000;
+
+    let app = routes::app::router();
+
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
+
+    println!("listening on {}", address);
+    axum::Server::bind(&address) // 주소 등록
+        .serve(app.into_make_service()) // 라우터 등록 및 실행
+        .await
+        .unwrap();
 }
